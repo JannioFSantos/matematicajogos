9cd65dd71506e705da9b9248390a556cd531d3fa
janniofsantos <jannioferreira@gmail.com>
2026-09-11 11:25:30 -0300

    porta 5000

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, jsonify, request
 from pathlib import Path
 from datetime import datetime
@@ -90,4 +91,5 @@ def limpar_pontuacoes():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', '5000'))
+    app.run(debug=False, host='0.0.0.0', port=port)
